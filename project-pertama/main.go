07b3bbcb20aca2db0e9e59ae2140ceb31ba93b26
ekokurniadi <ekokurniadi.02@gmail.com>
2021-08-16 09:44:31 +0700

    menambahkan keterangan / penjelasan coding pada project pertama

--- project-pertama/main.go
+++ project-pertama/main.go
@@ -5,3 +5,19 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+/*
+Keterangan :
+ 1. Package main => Didalam pemrograman golang wajib memiliki 1 file dengan nama package main. Package main ini disesuaikan dengan
+ nama file yang telah kita buat yaitu main.go
+ 2. Import "fmt" => Library untuk menampilkan / mengeprint sesuatu ke terminal atau pun browser
+ 3. func main => func main disini merupakan function utama yang akan dijalan kan oleh compiler go untuk pertama kali ketika dijalankan
+ 4. fmt.Println("Hello World") => Kita akan menampilkan atau mengeprint tulisan `Hello World` di CMD ketika kita menjalankan program ini
+ 5. Cara menjalankan program golang =>
+ - Buka terminal CMD / terminal yang ada pada visual studio code teman-teman
+ - Masuk ke directory project-pertama
+ - Kemudian ketikan command : go run main.go
+ - Kemudian akan tampil Tulisan `Hello World` pada terminal teman-teman
+ - Selamat mencoba :)
+
+*/
